ab7079dbbe8097983f4041db72d962d253c3ad42
djohoe28 <djohoe28@gmail.com>
2024-09-27 16:17:59 +0300

    SHARED - Linting

--- packages/shared/types/VideoActionType.ts
+++ packages/shared/types/VideoActionType.ts
@@ -1,5 +1,7 @@
-export enum VideoActionType {
-    PLAY = "PLAY",
-    PAUSE = "PAUSE",
-    SEEK = "SEEK",
+export enum VideoActionTypes {
+  PLAY,
+  PAUSE,
+  SEEK,
 }
+
+export type VideoActionType = keyof typeof VideoActionTypes;
