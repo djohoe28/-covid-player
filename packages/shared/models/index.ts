84c15d1ea95b7c59da1a025dd4e7da22742abdb5
djohoe28 <djohoe28@gmail.com>
2024-09-28 16:27:56 +0300

    SHARED - Removed named exports

--- packages/shared/models/index.ts
+++ packages/shared/models/index.ts
@@ -1,6 +1,6 @@
-export * as UserDTO from "./UserDTO";
-export * as SourceDTO from "./SourceDTO";
-export * as VideoState from "./VideoState";
-export * as PacketDTO from "./PacketDTO";
-export * as ChatPacketDTO from "./ChatPacketDTO";
-export * as VideoPacketDTO from "./VideoPacketDTO";
+export * from "./UserDTO";
+export * from "./SourceDTO";
+export * from "./VideoState";
+export * from "./PacketDTO";
+export * from "./ChatPacketDTO";
+export * from "./VideoPacketDTO";
