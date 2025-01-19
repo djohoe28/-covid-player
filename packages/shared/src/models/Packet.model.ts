3327399eba871ad9793eb82a9e0f736379545ea0
djohoe28 <djohoe28@gmail.com>
2025-01-19 15:28:24 +0200

    Packet model documentation

--- packages/shared/src/models/Packet.model.ts
+++ packages/shared/src/models/Packet.model.ts
@@ -1,8 +1,20 @@
 import type { PacketKind } from "@shared/enums/PacketKind.enums";
 
 export default interface Packet {
+	/**
+	 * Enum indicating the packet's purpose.
+	 */
 	kind: PacketKind;
+	/**
+	 * The sender's UUID.
+	 */
 	sender: string;
+	/**
+	 * The Unix timestamp of initial creation.
+	 */
 	timestamp: number;
+	/**
+	 * The payload; type can be inferred via {@link kind}.
+	 */
 	data: any;
 }
