8641372b5c883d125df1995f03ec0f37bf638de1
djohoe28 <djohoe28@gmail.com>
2025-02-03 17:12:57 +0200

    DAO interface linting

--- packages/shared/src/interfaces/DAO.interface.ts
+++ packages/shared/src/interfaces/DAO.interface.ts
@@ -1,24 +1,24 @@
 /**
  * Data Access Object (DAO) interface.
- * 
+ *
  * @todo // TODO: Replace T with DTO<T> ?
- * 
+ *
  * @see https://en.wikipedia.org/wiki/Data_access_object
- * 
+ *
  * @template T Type of item to store in database.
  * @template K Type of key to use to identify items in database.
  */
 export default interface DAO<T, K> {
 	/**
 	 * Creates a copy of the given item in the database, and returns it.
-	 * 
+	 *
 	 * @param item Item to create (add to database).
 	 * @returns The created item.
 	 */
 	create(item: T): Awaited<T>;
 	/**
 	 * Reads the item with the given key from the database, and returns it.
-	 * 
+	 *
 	 * @param key Key to read item from database.
 	 * @returns The item with the given key.
 	 */
@@ -27,11 +27,11 @@ export default interface DAO<T, K> {
 	 * Updates the given item in the database, and returns it;
 	 * - If a key is provided: the item with that key will be updated.
 	 * - If a key is not provided: the item's key will be used.
-	 * 
+	 *
 	 * @todo // TODO: What happens if the given item's key is different from the key parameter?
-	 * 
+	 *
 	 * @example [{ key: 1, name: "Alice" }] -> update({ key: 1, name: "Bob" }, 2) -> [{ key: 2, name: "Bob" }]
-	 * 
+	 *
 	 * @param item Item to update in database.
 	 * @param key Key to update item in database.
 	 * @returns The updated item.
@@ -39,28 +39,28 @@ export default interface DAO<T, K> {
 	update(item: T, key?: K): Awaited<T>;
 	/**
 	 * Deletes the given item from the database, and returns it.
-	 * 
+	 *
 	 * @param item Item to delete from database.
 	 * @returns The deleted item.
 	 */
 	delete(item: T): Awaited<T>;
 	/**
 	 * Deletes the item with the given key from the database, and returns it.
-	 * 
+	 *
 	 * @param key Key of item to delete from database.
 	 * @returns The deleted item.
 	 */
 	delete(key: K): Awaited<T>;
 	/**
 	 * Deletes the given item - or the item with the given key - from the database, and returns it.
-	 * 
+	 *
 	 * @param itemOrKey Item or key to delete from database.
 	 * @returns The deleted item.
 	 */
 	delete(itemOrKey: T | K): Awaited<T>;
 	/**
 	 * Reads all items from the database, and returns them.
-	 * 
+	 *
 	 * @returns All items in the database.
 	 */
 	readAll(): Awaited<T[]>;
